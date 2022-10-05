fbad1dfd8d932a917d29a1a6926bf9042d19ac3e
M1nt-y <sviatoslav.ivashchenko@student.sumdu.edu.ua>
2022-10-05 16:32:04 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,113 @@
+const body = document.body;
+const burger = document.getElementById('burger-menu');
+const menu = document.getElementById('dropdown-menu');
+
+burger.addEventListener('change', e => {
+    if(e.target.checked === true) {
+        menu.classList.toggle('active');
+        body.classList.toggle('hidden');
+    }
+    if(e.target.checked === false) {
+        menu.classList.remove('active');
+        body.classList.remove('hidden');
+    }
+});
+
+const nft = new Swiper('.top-nft__content-swiper', {
+    grabCursor: true,
+    width: 1370,
+    slidesPerView: 3,
+    freeMode: true,
+    breakpoints: {
+        320: {
+            slidesPerView: 1,
+            width: 250,
+        },
+        549: {
+          slidesPerView: 1,
+          width: 435,
+        },
+        935: {
+          slidesPerView: 2,
+          width: 935,
+        },
+        1370: {
+            width: 1370,
+            slidesPerView: 3,
+        }
+    }
+});
+
+const profile = new Swiper('.profiles__swiper', {
+    grabCursor: true,
+    width: 1370,
+    slidesPerView: 3,
+    freeMode: true,
+    breakpoints: {
+        320: {
+            slidesPerView: 1,
+            width: 270,
+        },
+        536: {
+          slidesPerView: 1,
+          width: 438,
+        },
+        935: {
+          slidesPerView: 2,
+          width: 935,
+        },
+        1370: {
+            width: 1370,
+            slidesPerView: 3,
+        }
+    }
+});
+
+const content = document.querySelectorAll('.collection');
+const tab = document.querySelectorAll('.collections__item');
+const slider = document.querySelector('.collections__items');
+
+function hideTabContent() {
+    content.forEach(item => {
+      item.style.display = 'none';
+    });
+    tab.forEach(item => {
+      item.classList.remove('collections__item--active');
+    });
+}
+function showTabContent(i = 0) {
+    content[i].style.display = 'block';
+    tab[i].classList.add('collections__item--active');
+}
+hideTabContent();
+showTabContent();
+slider.addEventListener("click", (e) => {
+    const target = e.target;
+    if (target) {
+        tab.forEach((item, i) => {
+            if (target == item) {
+                hideTabContent();
+                showTabContent(i);
+            }
+        });
+    }
+});
+
+
+const list = document.querySelectorAll('.footer__list-title');
+const items = document.querySelectorAll('.footer__top-list');
+
+list.forEach((item, i) => {
+    item.addEventListener("click", (e) => {
+        const target = e.target.closest('.footer__list-title');
+        if (target == item) {
+            items[i].classList.toggle('active');
+            items.forEach((listItem, j) => {
+                if (j != i && items[j].classList.contains('active')) {
+                    listItem.classList.toggle('active');
+                }
+            })
+        }
+    })
+});
+
